be7654640ece1d9696cbe74fd9d599afc3fa190c
franklin <franklin@weareinteractive.com>
2013-12-23 16:43:10 +0000

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'franklin@weareinteractive.com'
 license          'MIT'
 description      'Manage basic global or user\'s ssh configurations.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 recipe           "ssh_user", "Empty recipe for including LWRPs"
 recipe           "ssh_user::data_bag", "Read data bag to include ssh configs"
 
